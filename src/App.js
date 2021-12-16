cdfcd047bae66bf1744036221d48723d637195ee
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-12-16 13:06:25 -0500

    Adds copy.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <main>
       <h1>Warren's Big New Money Making Enterprise</h1>
+      <p>This is some new text that does other things.</p>
       <button>CLICK GIVE ME A BAG OF MONEY</button>
     </main>
   );
